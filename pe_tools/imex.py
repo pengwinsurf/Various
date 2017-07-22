b9d4d5e4413a9cd9d03c2edfebf8643c655edd81
Pengwinsurf <ahmedmzaki@gmail.com>
2017-07-22 02:41:49 +0100

    added version info dumping

--- pe_tools/imex.py
+++ pe_tools/imex.py
@@ -1,15 +1,18 @@
-/*
- * @Author: Pengwinsurf 
- * @Date: 2017-07-21 16:08:49 
- * @Last Modified by:   Pengwinsurf 
- * @Last Modified time: 2017-07-21 16:08:49 
- */
+#!/usr/bin/python 
+
+""" Script to extract information from a PE file """
+
 import argparse
-import pefile
 import os
+import pefile
 
+__author__ = "Pengwinsurf"
+__copyright__ = "Copyright 2017"
+__license__ = "MIT"
+__version__ = "0.2"
+__date__ = "22 July 2017"
 
-class IMEX():
+class PeInfo():
 
     def __init__(self, filePath):
         self.pe = pefile.PE(os.path.abspath(filePath))
@@ -24,6 +27,7 @@ class IMEX():
 
         for item in table:
             print "%d\t0x%08x\t%s" % (item[0], item[1], item[2])
+        print "--------"
 
 
     def _parse_directory_descriptor(self, structType, dirEntry):
@@ -58,18 +62,33 @@ class IMEX():
 
         return exportNames
 
+    def _print_verinfo(self):
+        """ Print version information is the file has any """
+
+        if hasattr(self.pe, 'FileInfo'):
+            print "\nVERSION INFORMATION"
+            for fileInfo in self.pe.FileInfo:
+                if fileInfo.Key == 'StringFileInfo':
+                    for vInfo in fileInfo.StringTable:
+                        for info in vInfo.entries.items():
+                            print "%s:\t%s" % (info[0], info[1])
+            print "--------"
+
     def run(self):
 
         if (self.impDirEnt.VirtualAddress != 0) and (self.impDirEnt.Size != 0):
-            print "\nImports:"
+            print "\nIMPORTS"
             for entry in self.pe.DIRECTORY_ENTRY_IMPORT:
                 print "\n%s" % entry.dll
                 for imp in entry.imports:
                     print "%s\t0x%08x\t%s" % (imp.ordinal, imp.address, imp.name)
+            print "--------"
 
         if (self.expDirEnt.VirtualAddress != 0) and (self.expDirEnt.Size != 0):
             self.exportDir = self._parse_directory_descriptor(self.pe.__IMAGE_EXPORT_DIRECTORY_format__, self.expDirEnt)
-            print "\nExports: "
+            print "\nDLL NAME\n%s" % self.pe.get_data(self.exportDir.Name).split('\x00')[0]
+            print "--------"
+            print "\nEXPORTS "
             print "\n%s\n" % self.exportDir
             addressOfFunctions = self._get_export_addresses()
             exportNames = self._get_export_names()
@@ -80,13 +99,15 @@ class IMEX():
 
             self._print_table(fullExports)
 
+        self._print_verinfo()
+
 def main():
 
     parser = argparse.ArgumentParser(description="Show imports and exports of a PE file")
     parser.add_argument('filename', help="Path to executable file to show imports/exports")
     options = parser.parse_args()
 
-    IMEX(options.filename).run()
+    PeInfo(options.filename).run()
 
 if __name__ == '__main__':
     main()
