0c86d9066ba2f80d2f9308d9a586f044b66ad4c4
pengwinsurf <ahmedmzaki@gmail.com>
2018-08-07 09:13:23 +0100

    added rsrc enumeration and pe structure info

--- pe_tools/peinfo.py
+++ pe_tools/peinfo.py
@@ -8,6 +8,7 @@ import binascii
 import imp
 import subprocess
 import re
+import datetime
 
 
 import pefile
@@ -22,10 +23,10 @@ except ImportError:
     OPENSSL = True
 
 __author__ = "Pengwinsurf"
-__copyright__ = "Copyright 2017"
+__copyright__ = "Copyright 2018"
 __license__ = "MIT"
-__version__ = "0.2"
-__date__ = "22 July 2017"
+__version__ = "0.3"
+__date__ = "07 Aug 2018"
 
 
 class PeInfo():
@@ -35,7 +36,66 @@ class PeInfo():
         self.pe = pefile.PE(os.path.abspath(filePath))
         self.expDirEnt = self.pe.OPTIONAL_HEADER.DATA_DIRECTORY[0]
         self.impDirEnt = self.pe.OPTIONAL_HEADER.DATA_DIRECTORY[1]
+    
+    def _read_pe_info(self):
+        """
+		Walks the PE sections loading standard info into the info dict and looking for anomalies
+		"""
+        print "Compile date: %s" % (datetime.date.fromtimestamp(self.pe.FILE_HEADER.TimeDateStamp))
+        if self.pe.is_dll():
+            print "File Type: DLL"
+        elif self.pe.is_driver():
+			print "File Type: Driver"
+        elif self.pe.is_exe():
+			print "File Type: EXE"
+        else:
+			print "Unidentified Type: %s" % hex(self.pe.FILE_HEADER.Characteristics)
 
+        print "Machine: %s" % pefile.MACHINE_TYPE[self.pe.FILE_HEADER.Machine]
+
+        print "Image base: 0x%08x" % self.pe.OPTIONAL_HEADER.ImageBase
+        print "Image size: 0x%08x" % self.pe.OPTIONAL_HEADER.SizeOfImage
+
+        print "EntryPoint RVA: 0x%08x" % self.pe.OPTIONAL_HEADER.AddressOfEntryPoint
+        print "EntryPoint in section: %s" % self.pe.get_section_by_rva(self.pe.OPTIONAL_HEADER.AddressOfEntryPoint).Name
+
+
+        print "Section\tVirtualAddress\tPhysicalAddress\tVirtualSize\tPhysicalSize\tFlags\tEntropy"
+
+        for section in self.pe.sections:
+            characteristics = ''            
+            if section.IMAGE_SCN_MEM_READ:
+                characteristics += 'r'
+            else:
+                characteristics += '_'
+            
+            if section.IMAGE_SCN_MEM_WRITE:
+                characteristics += 'w'
+            else:
+                characteristics += '_'
+            
+            if section.IMAGE_SCN_MEM_EXECUTE:
+                characteristics += 'x'
+            else:
+                characteristics += '_'
+            
+            if section.IMAGE_SCN_CNT_CODE:
+                characteristics += 'c'
+            else:
+                characteristics += '_'
+
+            print "%s\t0x%08x\t0x%08x\t0x%08x\t0x%08x\t%s\t%d" % (section.Name, section.VirtualAddress, section.PointerToRawData,\
+                    section.Misc_VirtualSize, section.SizeOfRawData, characteristics, section.get_entropy())
+        
+        pe_size = self.pe.sections[-1].PointerToRawData + self.pe.sections[-1].SizeOfRawData
+        ondisk_size = os.stat(self.filePath).st_size
+        print "\n"
+
+        if pe_size < ondisk_size:
+            appended_data_sz = ondisk_size - pe_size
+            print "Appended data offset\tSize"
+            print "0x%08x\t0x%08x" % (pe_size, appended_data_sz)
+    
     def _print_table(self, table):
         """ Helper function to print tables.
         Columns are Ordinal , VA, ExportName
@@ -91,9 +151,8 @@ class PeInfo():
                             print "%s:\t%s" % (info[0], info[1])
         else:
             print "[!]No Version Information"
-        print "--------"
 
-    def __print_debug_data(self):
+    def _print_debug_data(self):
         """ Print out Debug data if present """
         print "[+]DEBUG INFO\n"
         if (hasattr(self.pe, 'DIRECTORY_ENTRY_DEBUG')):
@@ -128,13 +187,10 @@ class PeInfo():
                             raw_pdb = binascii.unhexlify(temp_hex)
                             print "[+]PDB file: %s\n" % raw_pdb.split('\x00')[0].encode('utf-8')
 
-                except AttributeError, KeyError:
+                except (AttributeError,KeyError):
                     pass
         else:
             print"[!]No Debug Information"
-
-
-        print "--------"
 
 
     def _get_signature_info(self):
@@ -146,22 +202,19 @@ class PeInfo():
 
         if (securityDir.VirtualAddress == 0 and securityDir.Size == 0):
             print "[!]No Authenticode found"
+            return 
 
         sigSize = securityDir.Size + securityDir.VirtualAddress
         sig = self.pe.write()[securityDir.VirtualAddress+8: sigSize]
-
-
-        # with open(self.filePath, 'rb') as fh:
-        #     fh.seek(fileOffset)
-        #     sig = fh.read(securityDir.Size)
-
 
         with open('cert.der', 'wb+') as fh:
             fh.write(sig)
             print "[+]Signature DER file dumped to cert.der"
 
         if not OPENSSL:
-            p7 = SMIME.PKCS7(m2.pkcs7_read_bio_der(bio._ptr()))
+            file_handle  = open("cert.der", 'r')
+            input_bio = BIO.File(file_handle)
+            p7 = SMIME.PKCS7(m2.pkcs7_read_bio_der(input_bio._ptr()))
             #p7 = SMIME.load_pkcs7('temp.sig')
 
             signers = p7.get0_signers(X509.X509_Stack())
@@ -170,6 +223,8 @@ class PeInfo():
                 print "Issuer: %s " % cert.get_issuer().as_text()
                 print "Not After: %s " % cert.get_not_after()
                 print "Subject: %s " % cert.get_subject().as_text()
+
+            file_handle.close()
         else:
             args = ['openssl', 'pkcs7', '-inform', 'DER', '-print_certs', '-text', '-in', 'cert.der']
             proc = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -190,15 +245,53 @@ class PeInfo():
                 print "Valid From: %s" % matches[-1][1]
                 print "Valid To: %s" % matches[-1][2]
                 print "Subject: %s" % matches[-1][3]
-                
 
+    def _read_resources(self):
+        print "[+] Resources information"
+        rsrc_dir = self.pe.OPTIONAL_HEADER.DATA_DIRECTORY[2]
+        rsrc_root = self.pe.parse_resources_directory(rva=rsrc_dir.VirtualAddress)
+        if rsrc_root:
+            print "Number of rsrcs: %s" % (rsrc_root.struct.NumberOfNamedEntries + rsrc_root.struct.NumberOfIdEntries)
+            depth = 0
+            self._walk_resources(rsrc_root,depth)
+			# log.info("****** Resources Strings *******")
+			# strings = self.pe.get_resources_strings()
+			# if not strings:
+			# 	log.info("No strings found")
+			# else:
+			# 	self.info['STRINGS_IN_RSRCS'] = True
+			# 	for string in strings:
+			# 		log.info("%s", string)
+        else:
+			print "[!] No resources found"
+			self.info["RSRC"] = None
 
-
-
-    
-        print "--------"
+    def _walk_resources(self,rsrc_dir,depth):
+        for entry in rsrc_dir.entries:
+            try:
+                rsrc_data = entry.data
+                print "Lang: %s" % pefile.LANG[rsrc_data.lang]
+                print "Size: 0x%08x" % rsrc_data.struct.Size
+                return
+            except AttributeError:
+                if depth == 0:
+                    if entry.name:
+                        print "Name: %s" % entry.name
+                    else:
+                        print "Resource ID %s - Type: %s" % (hex(entry.id), pefile.RESOURCE_TYPE[entry.id])
+                else:
+					if entry.name:
+						print "Child Resource Name: %s" %  entry.name
+					else:
+						print "Child Resource ID:  %s" %  entry.id
+            depth += 1
+            self._walk_resources(entry.directory,depth)
+            depth = depth - 1
 
     def run(self):
+
+        self._read_pe_info()
+        print "--------"
 
         if (self.impDirEnt.VirtualAddress != 0) and (self.impDirEnt.Size != 0):
             print "[+]IMPORTS"
@@ -224,10 +317,12 @@ class PeInfo():
             self._print_table(fullExports)
 
         self._print_verinfo()
-
-        self.__print_debug_data()
-
+        print "--------"
+        self._print_debug_data()
+        print "--------"
         self._get_signature_info()
+        print "--------"
+        self._read_resources()
 
 def main():
 
